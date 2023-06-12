72af6cc701b7fa11744394e20a439f8ead0b1a4f
Marco Daniel <marco11.mendia@gmail.com>
2023-06-12 14:55:45 -0600

    Siguiente commit

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"runtime"
 
 	"github.com/Yuk3S4/godesde0/variables"
 )
@@ -10,4 +11,10 @@ func main() {
 	estado, texto := variables.ConviertoaTexto(1588)
 	fmt.Println(estado)
 	fmt.Println(texto)
+	// os := runtime.GOOS - Obtener sistema operativo actual
+	if os := runtime.GOOS; os == "Linux." || os == "OS X." {
+		fmt.Println("Esto no es Windows")
+	} else {
+		fmt.Println("Esto es Windows")
+	}
 }
